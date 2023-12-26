0fd78dd49820be5640ec166d6f1fb59dabcd7df1
sOkam <7308253+heysokam@users.noreply.github.com>
2023-12-26 21:28:31 +0100

    new: Hugo + Tailwind base site

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./content/**/*.{html,js}",
+    "./layouts/**/*.{html,js}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
