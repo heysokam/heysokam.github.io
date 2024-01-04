77eb0f85240cf8df6fbe94f3b12e9088dec7f55e
sOkam <7308253+heysokam@users.noreply.github.com>
2024-01-04 17:42:50 +0100

    new: Docusaurus+Tailwind setup

---
+++ src/docusaurus/docusaurus.config.ts
@@ -0,0 +1,134 @@
+import {themes as prismThemes} from 'prism-react-renderer';
+import type {Config} from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
+
+const realName :string= 'Ivan Mar'
+const tag      :string= 'Game & Engine Dev'
+const me       :string= 'heysokam'
+const gh       :string= '.github.io'
+const domain   :string= `${me}${gh}`
+
+const config: Config = {
+  title: realName,
+  tagline: tag,
+  favicon: 'img/favicon.ico',
+
+  // Set the production url of your site here
+  url: `https://${domain}`,
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: me, // Usually your GitHub org/user name.
+  projectName: domain, // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'throw',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          sidebarPath: './sidebars.ts',
+          // editUrl:  // Remove this to remove the "edit this page" links. Please change this to your repo.
+          //   'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        blog: 
+        {
+          showReadingTime: true,
+          // editUrl:  // Remove this to remove the "edit this page" links. Please change this to your repo.
+          //   'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    // Replace with your project's social card
+    image: 'img/docusaurus-social-card.jpg',
+    navbar: {
+      title: `${realName} | ${tag}`,
+      logo: {
+        alt: 'My Site Logo',
+        src: 'img/logo.svg',
+      },
+      items: [
+        {type: 'docSidebar', sidebarId: 'tutorialSidebar', position: 'right', label: 'Tutorial', },
+        {to: '/blog', label: 'Blog', position: 'right'},
+        {href: 'https://github.com/heysokam', label: 'GitHub', position: 'right', },
+      ],
+    },
+    footer: {
+      style: 'light',
+      // links: [
+      //   {
+      //     title: 'Docs',
+      //     items: [
+      //       {
+      //         label: 'Tutorial',
+      //         to: '/docs/intro',
+      //       },
+      //     ],
+      //   },
+      //   {
+      //     title: 'Community',
+      //     items: [
+      //       {
+      //         label: 'Discord',
+      //         href: 'https://discordapp.com/invite/docusaurus',
+      //       },
+      //     ],
+      //   },
+      //   {
+      //     title: 'More',
+      //     items: [
+      //       {
+      //         label: 'Blog',
+      //         to: '/blog',
+      //       },
+      //     ],
+      //   },
+      // ],
+      copyright: `Copyright © ${new Date().getFullYear()} Ivan Mar. CC-BY-SA-NC`,
+    },
+    colorMode: {
+      defaultMode: 'dark',
+      disableSwitch: true,
+      respectPrefersColorScheme: false,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+  plugins: [
+    // TailwindCSS processor plugin
+    async function myPlugin(context, options) {
+      return {
+        name: "docusaurus-tailwindcss",
+        configurePostCss(postcssOptions) {
+          // Appends TailwindCSS and AutoPrefixer.
+          postcssOptions.plugins.push(require("tailwindcss"));
+          postcssOptions.plugins.push(require("autoprefixer"));
+          return postcssOptions;
+        },
+      };
+    },
+  ],
+};
+
+export default config;
