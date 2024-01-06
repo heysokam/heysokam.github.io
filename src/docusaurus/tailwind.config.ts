1a1e59edb5e3921bab545c9eed01ebe44d69858f
Ivan Mar (sOkam!) <7308253+heysokam@users.noreply.github.com>
2024-01-06 08:11:48 -0800

    fix: Tailwind CSS defaults collided and broke docusaurus styling

--- src/docusaurus/tailwind.config.ts
+++ src/docusaurus/tailwind.config.ts
@@ -1,7 +1,14 @@
 import type { Config } from "tailwindcss"
+import { docDir } from './docusaurus.config.ts'
+
 
 export default {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Default tailwindcss setting
+    `./${docDir}/**/*.{md,mdx}`,  // Add markdown content to the tailwindcss processor
+  ],
+  darkMode: ['class', '[data-theme="dark"]'], // Hook into docusaurus dark mode settings
+  corePlugins: { preflight: false },          // Disable preflight. It breaks docusaurus css defaults. https://tailwindcss.com/docs/preflight
   plugins: [],
   theme: {
     extend: {},
